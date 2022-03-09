689422463fdd4008f0e76ee311c5e3fc9dde316b
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-09 12:09:58 +0530

    Added More Methods to Library

--- src/service/CarParking.java
+++ src/service/CarParking.java
@@ -1,11 +1,23 @@
 package service;
 
-import model.ParkingLot;
+import model.*;
 
 public interface CarParking {
 
-    void showPathToParkACar(String path, ParkingLot parkingLot, int row, int col);
+    Car createCar(String carNo, String carBrand, String carModel);
 
-    void showPathToExitACar(String path, ParkingLot parkingLot, int row, int col);
+    CarEntryExit checkAndGetLastCarParkingPlace(Car car);
+
+    void parkACar(MultiFloorCarParking obj, ParkingLot parkingLot, int[] position, Car car);
+
+    void exitACarFromPosition(MultiFloorCarParking obj, ParkingLot parkingLot, int[] pos, Car car);
+
+    long calculateParkingTimeInSeconds(ParkingCell parkingCell);
+
+    double generateBill(ParkingCell parkingCell, Car car, long seconds);
+
+    void generatePathToParkACar(MultiFloorCarParking obj, ParkingLot parkingLot, int[] position);
+
+    void generatePathToExitACar(MultiFloorCarParking obj, ParkingLot parkingLot, int[] pos);
 
 }
