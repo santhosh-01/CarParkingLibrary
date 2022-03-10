436e0ab6199a33e626d26b01d7f37c0aa4e10414
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-10 16:17:34 +0530

    Added new Interface "DataPrinter" to print the results and other sentences in any format at application end

---
+++ src/service/DataPrinter.java
@@ -0,0 +1,49 @@
+package service;
+
+import model.Car;
+import model.CarEntryExit;
+import model.MultiFloorCarParking;
+import model.ParkingLot;
+
+import java.time.LocalTime;
+
+public interface DataPrinter {
+
+    void showCarDetails(String carNo, String carBrand, String carModel);
+
+    void CarParkingCancelledMessage();
+
+    void LastCarParkingDetails(int[] pos, CarEntryExit carEntryExit);
+
+    void emptyCarParkingPlace(int[] pos);
+
+    void parkingConfirmation();
+
+    void yesOrNoInvalidMessage();
+
+    void parkingIsFullInFloor(int floor);
+
+    void printParkingAvailableFloors(MultiFloorCarParking obj);
+
+    void askCorrectFormatOfParkingPlace();
+
+    void invalidParkingPlace();
+
+    void generateReceipt(CarParking carParking, MultiFloorCarParking obj, ParkingLot parkingLot,
+                         int[] pos, Car car);
+
+    void noCarsAvailable();
+
+    void carNumberNotFound();
+
+    void givenCarNotInParking();
+
+    void askingBackToMainMenu();
+
+    void carParkingHistory(LocalTime time1, LocalTime time2, int[] pos, CarEntryExit carEntryExit);
+
+    void showCarInformation(Car car);
+
+    void duplicateCarExist();
+
+}
