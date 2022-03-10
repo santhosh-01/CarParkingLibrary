29964c5ecca1a0bf98b83490082903382d36b360
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-10 16:13:00 +0530

    Added More Core Methods for the Application

--- src/service/CarParking.java
+++ src/service/CarParking.java
@@ -2,22 +2,44 @@ package service;
 
 import model.*;
 
+import java.util.ArrayList;
+
 public interface CarParking {
+
+    Car acceptCarDetailsToPark();
 
     Car createCar(String carNo, String carBrand, String carModel);
 
+    boolean confirmCarDetails(Car car);
+
     CarEntryExit checkAndGetLastCarParkingPlace(Car car);
 
-    void parkACar(MultiFloorCarParking obj, ParkingLot parkingLot, int[] position, Car car);
+    boolean checkAndSuggestLastCarParkingPlace(Car car);
 
-    void exitACarFromPosition(MultiFloorCarParking obj, ParkingLot parkingLot, int[] pos, Car car);
+    ParkingLot suggestAndGetCarParkingLot();
+
+    int[] suggestAndGetParkingPlace(ParkingLot parkingLot);
+
+    void parkACar(ParkingLot parkingLot, int[] position, Car car);
+
+    Car acceptCarDetailsToExit();
+
+    boolean confirmCarDetailsForExit(Car car);
+
+    ParkingCell exitACarFromPosition(ParkingLot parkingLot, int[] pos, Car car);
 
     long calculateParkingTimeInSeconds(ParkingCell parkingCell);
 
     double generateBill(ParkingCell parkingCell, Car car, long seconds);
 
-    void generatePathToParkACar(MultiFloorCarParking obj, ParkingLot parkingLot, int[] position);
+    void generatePathToParkACar(ParkingLot parkingLot, int[] position);
 
-    void generatePathToExitACar(MultiFloorCarParking obj, ParkingLot parkingLot, int[] pos);
+    void generatePathToExitACar(ParkingLot parkingLot, int[] pos);
+
+    Car getValidCarNumberInParkingHistory();
+
+    void showCarParkingHistory(Car car);
+
+    boolean checkDuplicateCarNoInParking(String carNo);
 
 }
