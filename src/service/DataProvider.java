dd743b3b974bcb0fd48040f12eef24c6b3da3b8b
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-10 16:18:18 +0530

    Added new Interface "DataProvider" to ask the input and get the input in any format at application end

---
+++ src/service/DataProvider.java
@@ -0,0 +1,36 @@
+package service;
+
+import model.Car;
+import model.ParkingLot;
+
+public interface DataProvider {
+
+    boolean acceptBillingAmount();
+
+    String validateAndGetCarModel();
+
+    String validateAndGetCarBrand();
+
+    String validateAndGetCarNumber();
+
+    String getCarConfirmation();
+
+    String getLastCarParkingConfirmation();
+
+    String getSuggestedParkingFloorConfirmation(int floorNo);
+
+    String getFloorNumber();
+
+    String getSuggestedParkingPlaceConfirmation(int[] position);
+
+    String getCarParkingPlace(ParkingLot parkingLot);
+
+    String getCarNumberToExit();
+
+    String givenCarConfirmation(Car car);
+
+    String getBackChoice();
+
+    String getCarNumberForCarHistory();
+
+}
