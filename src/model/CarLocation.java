8bd0931bdb90b560a82d2e01ef7b597d22327ad8
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-18 12:12:43 +0530

    Added new class CarLocation to limit constraints

---
+++ src/model/CarLocation.java
@@ -0,0 +1,36 @@
+package model;
+
+public class CarLocation {
+
+    private CarParkingPlace carParkingPlace;
+    private int floorNo;
+
+    public CarLocation() {
+    }
+
+    public CarLocation(CarParkingPlace carParkingPlace, int floorNo) {
+        this.carParkingPlace = carParkingPlace;
+        this.floorNo = floorNo;
+    }
+
+    public CarLocation(int row, int col, int floorNo) {
+        this.carParkingPlace = new CarParkingPlace(row,col);
+        this.floorNo = floorNo;
+    }
+
+    public CarParkingPlace getCarParkingPlace() {
+        return carParkingPlace;
+    }
+
+    public void setCarParkingPlace(CarParkingPlace carParkingPlace) {
+        this.carParkingPlace = carParkingPlace;
+    }
+
+    public int getFloorNo() {
+        return floorNo;
+    }
+
+    public void setFloorNo(int floorNo) {
+        this.floorNo = floorNo;
+    }
+}
